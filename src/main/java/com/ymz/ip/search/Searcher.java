4feb80c371b387ed4c66d87918a3727ef62c04c7
范超 <fanchao@e.hunantv.com>
2020-05-28 16:36:39 +0800

    支持从资源目录中加载'search.db'文件到内存的方法

--- src/main/java/com/ymz/ip/search/Searcher.java
+++ src/main/java/com/ymz/ip/search/Searcher.java
@@ -8,9 +8,7 @@ import com.ymz.ip.utils.GZipUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.lucene.util.RamUsageEstimator;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +16,16 @@ import java.util.List;
 /**
  * Searcher
  *
- * @author ymz
- * @date 2019-12-16 02:13
+ * @author fc
+ * @date 2020-05-25
  **/
 @Slf4j
 public class Searcher implements IpSearchConstant {
+
     private static final Object LOCK = new Object();
     private static boolean memory_mode_load = false;
     private static boolean firstSearch = true;
+
     /**
      * for memory mode
      */
@@ -107,6 +107,7 @@ public class Searcher implements IpSearchConstant {
         return high;
     }
 
+
     /**
      * load data into memory
      */
@@ -150,6 +151,42 @@ public class Searcher implements IpSearchConstant {
 
     }
 
+    /**
+     * 用于从资源文件的字节流中载入IP文件
+     */
+    public static void myLoadFileToMemoryMode() {
+
+        long sTime = System.currentTimeMillis();
+        InputStream initialStream = Searcher.class.getResourceAsStream("/search.db");
+        if (initialStream == null) {
+            System.out.println("未读取到资源文件");
+        }
+
+
+        try {
+            //资源转为流读取
+            byte[] ipStream = toByteArray(initialStream);
+            System.out.println("流式读取字节数为" + ipStream.length);
+
+            byte[] headBlock = getByteArr(ipStream, 0, HEAD_BLOCK_LENGTH);
+            long dataEndPtr = ByteUtil.get32Long(headBlock, 0);
+            long ipSegmentsEndPrt = ByteUtil.get32Long(headBlock, 4);
+            int dataLen = (int) dataEndPtr;
+            byte[] searchInfoBytes = new byte[(int) (ipSegmentsEndPrt - dataEndPtr)];
+            dataRegion = getByteArr(ipStream, 0, dataLen);
+
+            getByteArr(ipStream, searchInfoBytes, (int)dataEndPtr, searchInfoBytes.length);
+            deserialization(searchInfoBytes);
+
+            long eTime = System.currentTimeMillis();
+            System.out.println("load file cost time: " + (eTime - sTime) + "ms");
+            memory_mode_load = true;
+        } catch (IOException o) {
+            throw new RuntimeException("load file error.", o);
+        }
+
+    }
+
     private static void deserialization(byte[] searchInfoBytes) {
         List<ArrayList> jsonArray = JSONArray.parseArray(new String(searchInfoBytes, StandardCharsets.UTF_8), ArrayList.class);
         ArrayList arrayList0 = jsonArray.get(0);
@@ -165,5 +202,44 @@ public class Searcher implements IpSearchConstant {
             ipRegionPtr[i] = (int) arrayList1.get(i);
             ipRegionLen[i] = (short) (int) arrayList2.get(i);
         }
+    }
+
+    private static byte[] toByteArray(InputStream input) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int n = 0;
+        while (-1 != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+        }
+        return output.toByteArray();
+    }
+
+    /**
+     * 从byte[]中抽取新的byte[]
+     * @param data - 元数据
+     * @param start - 开始位置
+     * @param end - 结束位置
+     * @return 新byte[]
+     */
+    public static byte[] getByteArr(byte[]data,int start ,int end){
+        byte[] ret=new byte[end-start];
+        for(int i=0;(start+i)<end;i++){
+            ret[i]=data[start+i];
+        }
+        return ret;
+    }
+
+    /**
+     * 从byte[]中抽取新的byte[]
+     * @param data - 元数据
+     * @param start - 开始位置
+     * @param len - 抽取长度
+     * @return 新byte[]
+     */
+    public static byte[] getByteArr(byte[]data, byte[] ret,  int start ,int len){
+        for(int i=0; i<len; i++){
+            ret[i]=data[start+i];
+        }
+        return ret;
     }
 }
